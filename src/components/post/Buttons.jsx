e55437e641beb775fdc58508a8cf39b60039839f
emreertugrl <emrertugrl@gmail.com>
2024-10-08 19:27:20 +0300

    isLiked added and remove to likes array with Firebase methods

--- src/components/post/Buttons.jsx
+++ src/components/post/Buttons.jsx
@@ -1,3 +1,4 @@
+import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 import {
   FaRetweet,
   FaRegHeart,
@@ -5,8 +6,24 @@ import {
   FaShareNodes,
   FaRegComment,
 } from "react-icons/fa6";
+import { auth, db } from "../../firebase";
 
-const Buttons = () => {
+const Buttons = ({ tweet }) => {
+  // oturumu açık olan kullanıcı likeladı mı
+  const isLiked = tweet.likes.includes(auth.currentUser.uid);
+
+  const toggleLike = async () => {
+    // güncellenecek döküman refereansı
+    const tweetRef = doc(db, "tweets", tweet.id);
+    // user id'sini likelamadıysa kaldır
+
+    // user id'sini likes dizisine ekle
+    await updateDoc(tweetRef, {
+      likes: isLiked
+        ? arrayRemove(auth.currentUser.uid)
+        : arrayUnion(auth.currentUser.uid),
+    });
+  };
   return (
     <div className="flex justify-between items-center">
       <div className="p-3 rounded-full cursor-pointer transition  hover:bg-blue-400/40">
@@ -17,8 +34,12 @@ const Buttons = () => {
         <FaRetweet />
       </div>
 
-      <div className="p-3 rounded-full cursor-pointer transition  hover:bg-red-400/30">
-        <FaRegHeart />
+      <div
+        onClick={toggleLike}
+        className="p-3 rounded-full cursor-pointer transition  hover:bg-red-400/30 flex items-center gap-2"
+      >
+        {isLiked ? <FaHeart className="text-red-500" /> : <FaRegHeart />}
+        {tweet.likes.length}
       </div>
 
       <div className="p-3 rounded-full cursor-pointer transition  hover:bg-gray-400/30">
